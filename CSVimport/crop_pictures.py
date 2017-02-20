2ca2628e828474514bf290b16f4bef873c4968b1
Noam Cohen <ncoh@post.bgu.ac.il>
2017-02-20 22:34:13 +0200

    added a crop file, for initial use

--- CSVimport/crop_pictures.py
+++ CSVimport/crop_pictures.py
@@ -7,20 +7,19 @@ import cv2 as ocv
 import itertools
 
 csv_path = "../TraxInputData/data.csv"
-probes_path = "../TraxInputData/Probes/"
-bottles_path = "../TraxInputData/Bottles/"
+probes_dir = "../TraxInputData/Probes/"
+bottles_dir = "../TraxInputData/Bottles/"
 
 
 class Bottle:
     def __init__(self, id, brand_code, brand_label, form_factor_label, mask, object_label, patch_id, patch_url,
-                 probe_id,
-                 product_label, voting_confidence):
+                 probe_id, product_label, voting_confidence):
         self.id = id
         self.brand_code = brand_code
         self.brand_label = brand_label
         self.form_factor_label = form_factor_label
         mask = mask.replace('\'', '\"')  # fix json string
-        self.mask = json.loads(mask)  # TODO: convert to json
+        self.mask = json.loads(mask)
         self.object_label = object_label
         self.patch_id = patch_id
         self.patch_url = patch_url
@@ -30,18 +29,13 @@ class Bottle:
         voting_confidence = voting_confidence.replace('u', '')
         self.voting_confidence = voting_confidence  # TODO: convert to json
 
-    def get_frame(self):
-        fr = json.loads(self.mask)
-        return fr
-
     def get_cropped_image(self):
-        image_path = probes_path + self.probe_id + ".jpg"
-        # frame = self.get_frame()
+        probe_path = probes_dir + self.probe_id + ".jpg"
         x1 = self.mask["x1"]
         x2 = self.mask["x2"]
         y1 = self.mask["y1"]
         y2 = self.mask["y2"]
-        img = ocv.imread(image_path)
+        img = ocv.imread(probe_path)
         cropped = img[y1:y2, x1:x2]
         return cropped
 
@@ -53,6 +47,6 @@ def import_data():
         for row in reader:
             bottle = Bottle(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10])
             cropped = bottle.get_cropped_image()
-            ocv.imwrite(bottles_path + bottle.patch_url, cropped)
+            ocv.imwrite(bottles_dir + bottle.patch_url, cropped)
 
 import_data()
